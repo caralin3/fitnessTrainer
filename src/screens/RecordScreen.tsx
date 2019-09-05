cce325cc541bd609b7faa7f5c63ee2ccc482e0f0
Cara Steinberg <csteinberg@geekhive.com>
2019-09-05 11:04:02 -0400

    Add first screen

---
+++ src/screens/RecordScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+// interface RecordScreenProps {}
+
+const DisconnectedRecordScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>Screen</Text>
+    </View>
+  );
+};
+
+export const RecordScreen = DisconnectedRecordScreen;
+
+const styles = StyleSheet.create({});
