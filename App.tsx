783e229bfe8d4ca1c4d0cdaf854dd6a74bc86096
Cara Steinberg <csteinberg@geekhive.com>
2019-09-05 16:15:35 -0400

    Add initial location implementation

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { RecordScreen } from './src/screens';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
       <RecordScreen />
     </View>
   );
--- src/screens/RecordScreen.tsx
+++ src/screens/RecordScreen.tsx
@@ -1,16 +1,86 @@
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import haversine from 'haversine';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 // interface RecordScreenProps {}
 
-const DisconnectedRecordScreen: React.FC = () => {
-  return (
-    <View>
-      <Text>Screen</Text>
-    </View>
-  );
-};
+interface RecordScreenState {
+  distance: number;
+  prevCoords: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
+class DisconnectedRecordScreen extends React.Component<{}, RecordScreenState> {
+  locationListener: { remove(): void } | null = null;
+
+  dist: number = 0;
+
+  state: RecordScreenState = {
+    distance: 0,
+    prevCoords: { latitude: 0, longitude: 0 }
+  };
+
+  componentDidMount() {
+    this._watchLocation();
+  }
+
+  componentWillUnmount() {
+    if (this.locationListener) {
+      this.locationListener.remove();
+    }
+  }
+
+  calcDist = (curCoords: { latitude: number; longitude: number }) => {
+    const { prevCoords } = this.state;
+    if (prevCoords.latitude && prevCoords.longitude) {
+      return haversine(prevCoords, curCoords, { unit: 'mile' });
+    }
+    return 0;
+  };
+
+  _watchLocation = async () => {
+    const { distance } = this.state;
+    this.locationListener = await Location.watchPositionAsync(
+      {
+        distanceInterval: 1,
+        accuracy: Location.Accuracy.Highest
+      },
+      location => {
+        const curCoords = {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude
+        };
+        this.dist = this.dist + this.calcDist(curCoords);
+        this.setState({
+          distance: distance + this.calcDist(curCoords),
+          prevCoords: curCoords
+        });
+      }
+    );
+  };
+
+  render() {
+    return (
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text>Screen</Text>
+          <Text>Distance: {this.dist.toFixed(4)} mi</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
 
 export const RecordScreen = DisconnectedRecordScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center'
+  }
+});
